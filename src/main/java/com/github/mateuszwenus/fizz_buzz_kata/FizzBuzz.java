500037246b99f3c4946da9ee463af10e0dc9fc1d
Mateusz Wenus <mateusz.wenus@gmail.com>
2016-08-30 22:06:43 +0200

    handle nonpositive numbers

--- src/main/java/com/github/mateuszwenus/fizz_buzz_kata/FizzBuzz.java
+++ src/main/java/com/github/mateuszwenus/fizz_buzz_kata/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.github.mateuszwenus.fizz_buzz_kata;
 public class FizzBuzz {
 
 	public String print(int number) {
+		if (number <= 0) {
+			throw new IllegalArgumentException("number must be positive");
+		}
 		boolean isFizz = number % 3 == 0;
 		boolean isBuzz = number % 5 == 0;
 		if (isFizz && isBuzz) {
